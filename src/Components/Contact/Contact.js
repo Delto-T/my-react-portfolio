a6de957e26cd8fddd5dc9892836af787438fc608
delto <tbo.z@hotmail.fr>
2023-02-15 22:05:51 +0100

    page contact initialisation

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,17 +1,41 @@
 // Library
 import React from "react";
-import styles from "./Contact.module.css"
+import styles from "./Contact.module.css";
+import imageContact from '../../images/imageContact1.png';
 
 // Component
-
+import ContactButton from "../ContactButton/ContactButton";
 
 function Contact()    {
+
+    // Varible
+
 
     // JSX
     return(
         <>
-        <div>
-            <h1>Contact test</h1>
+        <div id={styles.mainContact}>
+            <div id={styles.columnLeft}>
+                <div id={styles.titleContact}>
+                    <h1>Contact</h1>
+                </div>
+                <div id={styles.textContact}>
+                    Envie de travailler ensemble ? N'hésite pas à me contacter !
+                </div>
+                <div>
+                <ContactButton/> 
+                </div>
+                
+            </div>
+            <div id={styles.columnRight}>
+                <div >
+                    <figure>
+                        <img src={imageContact} alt="Image d'un ordinateur d'un developpeur web" id={styles.imgContact}/>
+                    </figure>
+                </div>
+
+            </div>
+            
         </div>
         </>
     );
